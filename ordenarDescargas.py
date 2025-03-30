4dc526eca0fd2706b255035c263e01be37b571c2
Tacopo <charlicant@gmail.com>
2025-03-31 00:52:07 +0200

    modificado para eliminar los archivos del escritorio y mandar los gcodes a su carpeta correspondiente

--- ordenarDescargas.py
+++ ordenarDescargas.py
@@ -21,6 +21,24 @@ def ordenar_descargas(directorio_descargas):
 
             shutil.move(ruta_archivo, os.path.join(carpeta_destino, archivo))
 
+    # Eliminar accesos directos y mover archivos gcode del escritorio
+    escritorio = os.path.expanduser("~/Desktop")
+    carpeta_gcodes = os.path.expanduser("~/Documents/gcodes")
+
+    if not os.path.exists(carpeta_gcodes):
+        os.makedirs(carpeta_gcodes)
+
+    for archivo in os.listdir(escritorio):
+        ruta_archivo = os.path.join(escritorio, archivo)
+
+        if os.path.isfile(ruta_archivo):
+            # Eliminar accesos directos (.lnk)
+            if archivo.lower().endswith(".lnk"):
+                os.remove(ruta_archivo)
+            # Mover archivos .gcode a la carpeta gcodes
+            elif archivo.lower().endswith(".gcode"):
+                shutil.move(ruta_archivo, os.path.join(carpeta_gcodes, archivo))
+
 if __name__ == "__main__":
     directorio_descargas = os.path.expanduser("~/Downloads")  # Cambiar si el directorio es diferente
     ordenar_descargas(directorio_descargas)
